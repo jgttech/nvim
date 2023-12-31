913f18efec113099da27b48269d6b880553e5f6b
Jonathon Tech <jgt.tech@protonmail.com>
2023-12-31 16:05:19 -0600

    Add toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,5 +1,19 @@
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
-  config = true,
+  config = import.config(function(use)
+    local ok, term = use({ "toggleterm" })
+
+    if ok then
+      term.setup({
+        open_mapping = [[<c-\>]],
+        direction = "float",
+        auto_scroll = true,
+        float_opts = {
+          border = "curved",
+          winblend = 3,
+        },
+      })
+    end
+  end),
 }
