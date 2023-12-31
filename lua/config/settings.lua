e9ecd5051ed70dafafe4bd0c674dc9d0e6afc4c1
Jonathon Tech <jgt.tech@protonmail.com>
2023-12-31 03:01:03 -0600

    Back out Nvim Tree, it is causing things to break

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,3 +1,7 @@
 -- Theme
 --vim.cmd("colorscheme kanagawa-dragon")
 vim.cmd("colorscheme kanagawa")
+
+-- recommended settings from nvim-tree documentation
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,64 @@
+return {}
+-- return {
+--   "nvim-tree/nvim-tree.lua",
+--   dependencies = { "nvim-tree/nvim-web-devicons" },
+--   config = import.config(function(use)
+--     local ok, nvimtree = use({ "nvim-tree" })
+--
+--     if ok then
+--       -- Change color for arrows in the tree
+--       vim.cmd([[ highlight NvimTreeFolderArrowClosed guifg=#7E9CD8 ]])
+--       vim.cmd([[ highlight NvimTreeFolderArrowOpen guifg=#7E9CD8 ]])
+--
+--       -- configure nvim-tree
+--       nvimtree.setup({
+--         view = {
+--           width = 35,
+--           relativenumber = true,
+--         },
+--         -- change folder arrow icons
+--         renderer = {
+--           indent_markers = {
+--             enable = true,
+--           },
+--           -- icons = {
+--           --   glyphs = {
+--           --     folder = {
+--           --       arrow_closed = "→", -- arrow when folder is closed
+--           --       arrow_open = "↓", -- arrow when folder is open
+--           --     },
+--           --   },
+--           -- },
+--         },
+--         -- disable window_picker for
+--         -- explorer to work well with
+--         -- window splits
+--         actions = {
+--           open_file = {
+--             window_picker = {
+--               enable = false,
+--             },
+--           },
+--         },
+--         filters = {
+--           custom = { ".DS_Store" },
+--         },
+--         git = {
+--           ignore = false,
+--         },
+--       })
+--
+--       keymap({
+--         { "n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" } },
+--         {
+--           "n",
+--           "<leader>ef",
+--           "<cmd>NvimTreeFindFileToggle<CR>",
+--           { desc = "Toggle file explorer on current file" },
+--         },
+--         { "n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" } },
+--         { "n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" } },
+--       })
+--     end
+--   end),
+-- }
