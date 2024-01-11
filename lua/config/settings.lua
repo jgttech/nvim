a768699f94a5c65f6e3f4b73fe5547b869803dc2
Jonathon Tech <jonathon.tech@icfnext.com>
2024-01-11 16:03:45 -0600

    theme switch

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -2,8 +2,8 @@
 -- vim.g.catppuccin_flavour = "mocha"
 -- vim.cmd.colorscheme("onedark")
 -- vim.cmd.colorscheme("catppuccin")
-vim.cmd.colorscheme("kanagawa-dragon")
--- vim.cmd.colorscheme("kanagawa")
+-- vim.cmd.colorscheme("kanagawa-dragon")
+vim.cmd.colorscheme("kanagawa")
 
 -- recommended settings from nvim-tree documentation
 vim.g.loaded_netrw = 1
