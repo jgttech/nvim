79523402b422509e5ad47595d47122c16ab05951
Jonathon Tech <jgt.tech@protonmail.com>
2024-01-24 22:39:11 -0600

    WIP Setup Mason LSP's with lsp-zero

--- lua/plugins/lazy/after/lsp-zero.lua
+++ lua/plugins/lazy/after/lsp-zero.lua
@@ -3,33 +3,39 @@
 -- plugin manager has been run.
 local lsp_zero = require('lsp-zero')
 
-lsp_zero.on_attach(function(client, bufnr)
-  lap_zero.default_keymaps({ buffer = bufnr })
+lsp_zero.on_attach(function(_, bufnr)
+  lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Options: https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers
   ensure_installed = {
-    'bash-language-server',
-    -- 'cssmodules-language-server',
-    -- 'deno',
-    -- 'dockerfile-language-server',
-    -- 'docker-compose-language-service',
-    -- 'elixir-ls',
-    -- 'goimports',
-    -- 'gopls',
-    -- 'html-lsp',
-    -- 'biome',
-    -- 'lua-language-server',
-    -- 'marksman',
-    -- 'prisma-language-server',
-    -- 'pyright',
-    -- 'taplo',
-    -- 'tailwindcss-language-server',
-    -- 'typescript-language-server',
-    -- 'yaml-language-server',
-    -- 'zls'
+    'bashls',
+    'cssmodules_ls',
+    'denols',
+    'diagnosticls',
+    'dockerls',
+    'docker_compose_language_service',
+    'eslint',
+    'elixirls',
+    'gopls',
+    'html',
+    'htmx',
+    'biome',
+    'tsserver',
+    'lua_ls',
+    'marksman',
+    'pyright',
+    'rust_analyzer',
+    'sqlls',
+    'taplo',
+    'tailwindcss',
+    'templ',
+    'terraformls',
+    'lemminx',
+    'yamlls',
+    'zls',
   },
   handlers = {
     lsp_zero.default_setup,
