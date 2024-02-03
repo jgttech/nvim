7f9747fb3a2691f33722d665de72af0928786e56
Jonathon Tech <jgt.tech@protonmail.com>
2024-02-02 23:54:36 -0600

    WIP update for better Tailwind CSS support and sorting classes, etc

--- lua/plugins/tailwind-sorter.lua
+++ lua/plugins/tailwind-sorter.lua
@@ -3,9 +3,5 @@ return {
   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
   build = "cd formatter && npm i && npm run build",
   lazy = true,
-  config = function()
-    require("tailwind-sorter").setup({
-      on_save_pattern = { "*.html", "*.jsx", "*.tsx", "*.js", ".ts" },
-    })
-  end,
+  config = true,
 }
