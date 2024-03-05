f0c814cb0325eeaf944bc4d380a1ca80b3468933
Jonathon Tech <jgt.tech@protonmail.com>
2024-03-05 15:26:22 -0600

    Add Treesitter configuration

--- lua/ide/plugins/treesitter.lua
+++ lua/ide/plugins/treesitter.lua
@@ -1,17 +1,65 @@
 -- Highlight, edit, and navigate code
 return {
   'nvim-treesitter/nvim-treesitter',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    'windwp/nvim-ts-autotag',
+  },
   build = ':TSUpdate',
   config = function()
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
     ---@diagnostic disable-next-line: missing-fields
     require('nvim-treesitter.configs').setup {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+      autotag = {
+        enable = true,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<C-space>',
+          node_incremental = '<C-space>',
+          scope_incremental = false,
+          node_decremental = '<bs>',
+        },
+      },
+      ensure_installed = {
+        'bash',
+        'c',
+        'comment',
+        'css',
+        'elixir',
+        'gitattributes',
+        'gitignore',
+        'gitcommit',
+        'go',
+        'html',
+        'javascript',
+        'typescript',
+        'markdown',
+        'json5',
+        'jsonc',
+        'lua',
+        'python',
+        'prisma',
+        'rust',
+        'sql',
+        'toml',
+        'tsx',
+        'yaml',
+        'vim',
+        'vimdoc',
+        'query',
+        'zig',
+      },
     }
 
     -- There are additional nvim-treesitter modules that you can use to interact
