72719664f21627164a5ad6b1dbbeabbd34462426
Jonathon Tech <jgt.tech@protonmail.com>
2024-03-05 23:14:50 -0600

    Add window split configuration

--- lua/ide/config/keymap.lua
+++ lua/ide/config/keymap.lua
@@ -55,3 +55,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 vim.keymap.set('n', '<leader>q', '<cmd>Bdelete this<cr>', { desc = 'Close the current buffer' })
 vim.keymap.set('n', '<leader>bo', '<cmd>Bdelete other<cr>', { desc = 'Close all buffers except current one' })
 vim.keymap.set('n', '<leader>bm', '<cmd>Bdelete menu<cr>', { desc = 'Interactively choose how to delete buffers' })
+
+-- Window management
+vim.keymap.set('n', '<leader>sv', '<C-w>v', { desc = 'Split window vertically' })
+vim.keymap.set('n', '<leader>sh', '<C-w>s', { desc = 'Split window horizontally' })
+vim.keymap.set('n', '<leader>se', '<C-w>=', { desc = 'Make splits equal size' })
+vim.keymap.set('n', '<leader>sx', '<cmd>close<CR>', { desc = 'Close current split' })
