d01498f82ceb3ca940365ce7d5106cdbc697a4ea
Jonathon Tech <jgt.tech@protonmail.com>
2024-03-07 13:42:21 -0600

    2024-03-07 13:42:20 - UPDATES

--- lua/ide/plugins/comment.lua
+++ lua/ide/plugins/comment.lua
@@ -10,5 +10,21 @@
 -- "gc" to comment visual regions/lines
 return {
   'numToStr/Comment.nvim',
-  opts = {},
+  dependencies = {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+  },
+  config = function()
+    local comment = require 'Comment'
+    local ts_context_comment = require 'ts_context_commentstring'
+    local comment_nvim = require 'ts_context_commentstring.integrations.comment_nvim'
+
+    ts_context_comment.setup {
+      enable = true,
+      enable_autocmd = false,
+    }
+
+    comment.setup {
+      pre_hook = comment_nvim.create_pre_hook(),
+    }
+  end,
 }
