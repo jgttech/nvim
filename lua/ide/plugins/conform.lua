74dbabce1df64197450b45cc83814d9d02b34b04
Jonathon Tech <jgt.tech@protonmail.com>
2024-03-07 09:26:42 -0600

    2024-03-07 09:26:41 - UPDATES

--- lua/ide/plugins/conform.lua
+++ lua/ide/plugins/conform.lua
@@ -1,32 +1,38 @@
 -- Autoformat
 return {
   'stevearc/conform.nvim',
-  opts = {
-    notify_on_error = false,
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      javascript = { { 'prettier', 'biome' } },
-      typescript = { { 'prettier', 'biome' } },
-      javascriptreact = { { 'prettier', 'biome' } },
-      typescriptreact = { { 'prettier', 'biome' } },
-      css = { { 'prettier', 'biome' } },
-      html = { { 'prettier', 'biome' } },
-      json = { { 'prettier', 'biome' } },
-      yaml = { { 'prettier', 'biome' } },
-      markdown = { { 'prettier', 'biome' } },
-      python = { { 'isort', 'black' } },
-      tailwindcss = { { 'rustywind' } },
-      go = { { 'gofmt', 'goimports' } },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
-      -- You can use a sub-list to tell conform to run *until* a formatter
-      -- is found.
-      -- javascript = { { "prettierd", "prettier" } },
-    },
-  },
+  config = function()
+    local conform = require 'conform'
+
+    conform.setup {
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        javascript = { { 'prettier', 'biome' } },
+        typescript = { { 'prettier', 'biome' } },
+        javascriptreact = { { 'prettier', 'biome' } },
+        typescriptreact = { { 'prettier', 'biome' } },
+        css = { { 'prettier', 'biome' } },
+        html = { { 'prettier', 'biome' } },
+        json = { { 'prettier', 'biome' } },
+        yaml = { { 'prettier', 'biome' } },
+        markdown = { { 'prettier', 'biome' } },
+        python = { { 'isort', 'black' } },
+        tailwindcss = { { 'rustywind' } },
+        go = { { 'gofmt', 'goimports' } },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = true,
+        timeout_ms = 500,
+      },
+    }
+
+    vim.keymap.set('n', '<leader>fb', function()
+      conform.format {
+        lsp_fallback = true,
+        async = true,
+        timeout_ms = 500,
+      }
+    end, { desc = 'Format file or range (in visual mode)' })
+  end,
 }
