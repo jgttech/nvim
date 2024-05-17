892a0a9b7dcc4246961c9db14187a032ef27fb23
Jonathon Tech <jgt.tech@protonmail.com>
2024-05-17 18:38:41 -0500

    2024-05-17 18:38:41 - UPDATES

--- lua/ide/config.lua
+++ lua/ide/config.lua
@@ -1,5 +1,9 @@
 local config = require 'ide.utils.autoload' 'ide.config'
 
+if vim.fn.has 'termguicolors' == 1 then
+  vim.o.termguicolors = true
+end
+
 -- Just add the name of the confguration file that needs
 -- to be loaded.
 config {
