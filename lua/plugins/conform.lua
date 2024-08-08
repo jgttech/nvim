5b32f4185f2abf72806011cb1ad8acc5741c177f
Jonathon Tech <jgt.tech@protonmail.com>
2024-08-07 19:38:21 -0500

    2024-08-07 19:38:21 - UPDATES

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,6 +1,5 @@
 -- Autoformat
 local merge = require 'utils.merge'
--- local print_table = require 'utils.print_table'
 
 return {
   'stevearc/conform.nvim',
@@ -12,26 +11,21 @@ return {
       formatters_by_ft = merge(require('lsp').conform, {
         lua = { 'stylua' },
       }),
-      format_on_save = {
-        lsp_format = "fallback",
-        timeout_ms = 500
+      default_format_opts = {
+        lsp_format = 'fallback',
+        quiet = true,
       },
-      -- Previous solution.
-      -- format_on_save = {
-      --   lsp_fallback = true,
-      --   timeout_ms = 500,
-      -- },
-      -- format_on_save = function(bufnr)
-      --   -- Disable "format_on_save lsp_fallback" for languages that don't
-      --   -- have a well standardized coding style. You can add additional
-      --   -- languages here or re-enable it for the disabled ones.
-      --   local disable_filetypes = { c = true, cpp = true }
-      --
-      --   return {
-      --     timeout_ms = 500,
-      --     lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-      --   }
-      -- end,
+      format_after_save = function(bufnr)
+        -- Disable "format_after_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+
+        return {
+          timeout_ms = 500,
+          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        }
+      end,
     }
 
     vim.keymap.set('n', '<leader>fb', function()
@@ -39,6 +33,7 @@ return {
         lsp_fallback = true,
         async = true,
         timeout_ms = 500,
+        quiet = true,
       }
     end, { desc = 'Format file or range (in visual mode)' })
   end,
